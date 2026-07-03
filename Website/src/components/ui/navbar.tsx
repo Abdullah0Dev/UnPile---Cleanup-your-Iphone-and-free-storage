d08c483a420b96174878213157ccf0dc95a9bb2f
Abdullah Dev <abdullahdev001@gmail.com>
2026-07-03 14:35:39 +0100

    alhamdullah - update navbar logo navigaiton link

--- Website/src/components/ui/navbar.tsx
+++ Website/src/components/ui/navbar.tsx
@@ -7,7 +7,7 @@ import React from "react";
 const Navbar = () => {
   return (
     <nav className=" z-20 max-sm:mx-auto mx-5 max-w-6xl flex items-center place-self-center justify-between rounded-2xl bg-black/40 fixed w-full  top-5 px-6 py-4 backdrop-blur-3xl border border-white/5 shadow-2xl">
-      <Link href={"#"} className="flex items-center  gap-0.5">
+      <Link href={"/#"} className="flex items-center  gap-0.5">
         {/* <div className="flex h-8 w-8 items-center justify-center rounded-xl bg-gradient-to-br from-[#8b5cf6] to-[#3b82f6] shadow-lg shadow-primary/20">
           <Sparkles className="h-4 w-4 text-white" strokeWidth={2.5} />
         </div>
