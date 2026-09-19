f7b10d8adb624fbf8bc75fd22c8c504f9baed0a8
Abdullah Dev <abdullahdev001@gmail.com>
2026-09-19 03:09:21 -0400

    alhamdullah - short link to download the app | update info.plist only with needed permissions

---
+++ Website/src/app/download/page.tsx
@@ -0,0 +1,8 @@
+import { redirect } from "next/navigation";
+
+const DownloadApp = () => {
+  redirect("https://apps.apple.com/us/app/unpile-clean-up-photos/id6813002849");
+  return <div>DownloadApp</div>;
+};
+
+export default DownloadApp;
